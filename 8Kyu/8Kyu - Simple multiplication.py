63c91167a89dcd6d1f05170531984da8b0517fa4
Andrey Vinkov <realwensent@mail.ru>
2022-05-17 21:16:37 +0300

    Several exercises from 8Kyu and 7Kyu + 6Kyu

---
+++ 8Kyu/8Kyu - Simple multiplication.py
@@ -0,0 +1,9 @@
+"""
+Simple multiplication
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+"""
+
+
+def simple_multiplication(number) :
+    return number * 8 if number % 2 == 0 else number * 9
